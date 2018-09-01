2c997de53ce5f2d9af7aff9993e03f821fb613f8
Ray <ray@raymeredith.com>
2018-08-31 22:35:54 -0500

    Completed ATBS Chapter 2

--- python/playground.py
+++ python/playground.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-import numpy as np
-
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+while True:
+    print('Please type your name.')
+    name = input()
+    if name == 'your name':
+        break
+print('Thank you!')
