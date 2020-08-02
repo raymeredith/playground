370a7d6b32971cfac16e18edd2475961184df72c
Ray <ray@raymeredith.com>
2020-08-01 23:03:45 -0500

    Add Monty Hall project

---
+++ python/montyhall2.py
@@ -0,0 +1,11 @@
+import random
+
+#def runTrial(switchDoors):
+doors = 3
+car = random.randint(1, doors)
+selection = random.randint(1, doors)
+#revealed = 
+print(car)
+print(selection)
+#print(revealed)
+
